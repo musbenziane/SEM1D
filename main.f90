831e1506c50e7c64919bd096ab34a3091308d4e4
musbenziane <medbenziane1992@gmail.com>
2021-12-01 15:53:37 +0100

    correct false push

--- main.f90
+++ main.f90
@@ -5,8 +5,9 @@ program SEM1D
     ! Spectral Elements 1D Solver with a regular mesh for the elastic wave equation 
     ! NOTE: This program is yet to be tested against analytical solutions.
     ! 
-    ! Sources:  Igel 2017, Leveque 2002, Hesthaven & Warburton 2008
+    ! Language: Fortran 90, with parralel impelementation using OpenMP API
     ! 
+    ! Sources:  Igel 2017
     ! 
     ! The code used for arbitrary GLL points and weights was created by M.I.T departement of engineering. Link is hereafter
     ! https://geodynamics.org/cig/doxygen/release/specfem3d/    | file name: gll_library.f90
@@ -16,23 +17,20 @@ program SEM1D
     ! Supervisor: Pr. Emmanual Chaljub
     ! Author    : Mus Benziane
     !
-    ! Input file: [Example] [The BC last options are not used yet in this version, they'll be added by 04/12/21]
+    ! Input file: [Example]
     ! testing              ! Model name prefix (model names: prefix_vp, prefix_rho)
-    ! 6                    ! Polynomial order
-    ! 200                  ! Number of elements
-    ! 30.                  ! Element size
-    ! 20.                  ! Wavelet's peak frequency
-    ! 0.0002               ! Time step
+    ! 3                    ! Polynomial order
+    ! 1000                 ! Number of elements
+    ! 20                   ! Element size
+    ! 5.                   ! Wavelet's peak frequency
+    ! 0.001                ! Time step
     ! 10000                ! Number of time steps
-    ! 2250                 ! Source location (element number)
-    ! 4                    ! Source location (gll point)
-    ! 10                   ! Snapshot interval
+    ! 1                    ! Source location (element number)
+    ! 1                    ! Source location (gll point)
     ! 1                    ! [1/2/3/4] 1: Free surface, 2: Rigid wall, 3: Periodic, 4: Sponge 
-    ! 0                    ! Boundary condition only on the left side [not for periodic BC | on the right side for absorbing]
+    ! 1                    ! Boundary condition only on the left side [not for periodic BC | on the right side for absorbing]
     ! 20                   ! Sponge layer width in gll points
     ! .65                  ! Att constant for sponge layer
-    ! 0                    ! Use intial conditions instead of point source, and produce analytical solution
-    ! 100                  ! Width of Gaussian used as intial condition
     ! 
     ! -> Model files in C-Style binary floats [doubles]: Vs, Rho files are needed.
     !                                                  : For simple models, use create1Dmodel_files.f90
